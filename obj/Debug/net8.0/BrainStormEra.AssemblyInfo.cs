aceb4e64aaa2769bc20e17e810504d83c7ccc8d9
LTP <lamphat279@gmail.com>
2024-09-10 14:00:52 +0700

    add ignore

--- obj/Debug/net8.0/BrainStormEra.AssemblyInfo.cs
+++ obj/Debug/net8.0/BrainStormEra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainStormEra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+407d2e92dc02bbcec3688b6b1198f42e7cefb5b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainStormEra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainStormEra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

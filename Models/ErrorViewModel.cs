17d340bc9b3ab4408b77c8bb5470d55611906025
LTP <lamphat279@gmail.com>
2024-09-10 10:04:32 +0700

    root folder

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrainStormEra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
